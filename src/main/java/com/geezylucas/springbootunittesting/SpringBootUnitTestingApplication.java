c9f45b72c1e1d3901cce7ddc649f96ad1cace078
Gerardo Lucas Cruz <geezylucas@Gerardos-MacBook-Air.local>
2022-10-13 00:09:50 -0500

    Initial project

---
+++ src/main/java/com/geezylucas/springbootunittesting/SpringBootUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.geezylucas.springbootunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestingApplication.class, args);
+	}
+
+}
